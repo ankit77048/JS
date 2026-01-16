bd8d537249afc46ecff6a8552500e415b3ab7922
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-16 14:36:52 +0000

    change in code

--- basicCode/02_dataType.js
+++ basicCode/02_dataType.js
@@ -4,3 +4,14 @@ let state=null
 let city;
 let admissionTake=false
 console.table([typeof accountName, typeof id, typeof state, typeof city , typeof admissionTake])
+const marks=[24,56,78,20]
+let myObj={
+    name:"hitesh",
+    age:34,
+}
+console.log(typeof marks, marks)
+console.log(typeof myObj, myObj)
+let myFun=function(){
+    console.log("hello")
+}
+console.log(typeof myFun, myFun)
