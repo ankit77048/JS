d6990d6b491bf3556499ee3111dfdd578b85816f
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-16 11:41:19 +0000

    da

---
+++ basicCode/02_dataType.js
@@ -0,0 +1,6 @@
+let accountName="hitesh"
+let id=12
+let state=null
+let city;
+let admissionTake=false
+console.table([typeof accountName, typeof id, typeof state, typeof city , typeof admissionTake])
