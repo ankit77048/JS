b8148c00ef50115e5c6d755b5c47be3c09f75fc8
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-16 11:34:38 +0000

    variable

---
+++ basicCode/01_variable.js
@@ -0,0 +1,8 @@
+const accountId=123445
+let accountEmail="ankit998@gamil.com"
+var accountPassword="23455"
+accountCity="beguluru"
+accountEmail="ajwabjb@gmailo.com"
+accountPassword="7828782"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
