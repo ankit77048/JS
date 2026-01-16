2dbf5fe468906e3616640ca4e44c5155e07f3650
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-16 12:12:31 +0000

    conver

---
+++ basicCode/03_Conversion.js
@@ -0,0 +1,12 @@
+let isString="33"
+let isNumber=Number(isString)
+console.log(typeof isNumber)
+console.log(isNumber)
+let isloggedString="abfdb"
+let isloggedNumber=Number(isloggedString)
+console.log(typeof isloggedNumber)
+console.log(isloggedNumber)
+let isLogged=true
+let isLoggedNum=Number(isLogged)
+console.log(typeof isLogged)
+console.log(isLoggedNum)
