3c9d0225d76a3321321dc91b70855242d3482b9e
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-17 12:32:14 +0000

    string

---
+++ basicCode/04_string.js
@@ -0,0 +1,20 @@
+const name="rameshYadav"
+const count=40
+console.log(name + count + "value")
+console.log(`My name is ${name} and my count is ${count}`)
+console.log(name[0])
+console.log(name.length)
+console.log(name.indexOf('Y'))
+const stringName=new String("hiteshShrama")
+console.log(stringName.charAt(6))
+console.log(stringName.substring(0,4))
+const newString=stringName.slice(-12,5)
+console.log(newString)
+const stringOne="     hitesh    "
+console.log(stringOne)
+console.log(stringOne.trim())
+console.log(stringOne.replace('i','a'))
+const stringTwo=new String("my-name-is-ankit-yadav")
+console.log(stringTwo.split("-"))
+console.log(stringTwo.includes('name'))
+
