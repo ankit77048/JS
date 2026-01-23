717a50693ac4457838a6244b19f3bbda21cea2ec
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-23 11:57:46 +0000

    ggg

---
+++ 02_basicCode/04_object.js
@@ -0,0 +1,9 @@
+const obj1={1:"a",2:"b",3:"c"}
+const obj2={4:"d",5:"e",6:"f"}
+// const obj3=(obj1 ,obj2 )
+const obj3=Object.assign({},obj1,obj2)
+console.log(obj3)
+const obj4={...obj1,...obj2}
+console.log(obj4)
+console.log(Object.keys(obj3))
+console.log(Object.entries(obj3))
