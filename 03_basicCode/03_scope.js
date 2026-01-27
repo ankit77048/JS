e1662398839523a7e376b2d8c20557f2f4f82b8e
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-27 14:24:56 +0000

    new

---
+++ 03_basicCode/03_scope.js
@@ -0,0 +1,13 @@
+let a=20
+const b=200
+var c=2000
+if(true){
+    let a=10
+    const b=100
+    var c=1000
+    console.log(`Inner value of ${a} `)
+    console.log(`Inner value of b ${b}`)
+}
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ 03_basicCode/04_arrowFunction.js
@@ -0,0 +1,9 @@
+const addTwo = ( num1, num2 ) => (num1+num2)
+console.log(addTwo(5,6))
+const addTwoNew = ( num1, num2 ) => {
+    return num1+num2
+}
+console.log(addTwoNew(5,7))
+
+const sum=() => ({name:"hitesh"})
+console.log(sum())
