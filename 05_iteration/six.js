943a388c1a6c5adf32f1d3ababec20c2f1ffdebe
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-30 14:04:36 +0000

    six

---
+++ 05_iteration/six.js
@@ -0,0 +1,11 @@
+const myNum=[1,2,3,4,5,6,7,8,9,10]
+// const newNum=myNum.filter((num)=> num>5)
+// console.log(newNum)
+ 
+ //or
+
+
+const newNum=myNum.filter((num)=> {
+    return num>5 
+})
+console.log(newNum)
