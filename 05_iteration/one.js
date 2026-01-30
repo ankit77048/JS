cb51133db03d130b26a23e5f41e4d3b41437147b
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-30 11:02:41 +0000

    one

---
+++ 05_iteration/one.js
@@ -0,0 +1,16 @@
+// here we print value by using for loop
+
+for(let i=0;i<=10;i++){
+    console.log(i)
+}
+
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(i + "*" + j + "=" + i*j)
+    }
+}
+
+let arr=["spiderman" , "superman " , "ironman"]
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
