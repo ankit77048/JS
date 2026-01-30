1ce68caac3c29951771805ddc5f68e7fedb2ca39
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-30 11:23:00 +0000

    two

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+// here i print value by using while loop
+
+let i=0
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+let arr=["thor" , "spiderman" , "captain america"]
+let j=0
+while(j<arr.length){
+    console.log(arr[j])
+    j++
+}
+
+// here i use do while loop to print value
+
+let score=1
+do{
+    console.log(`score is ${score}`)
+    score++
+}
+while(score<=10)
