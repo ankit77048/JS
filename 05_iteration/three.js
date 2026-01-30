8447da74887f83521c008e42c7dcafbad6e356c4
Ankit Yadav <ankit.official7704@gmail.com>
2026-01-30 12:36:47 +0000

    three

---
+++ 05_iteration/three.js
@@ -0,0 +1,18 @@
+// we print array by using fo rof loop
+
+// const arr=[1,2,3,4,5]
+// for(const ele of arr){
+//     console.log(ele)
+// }
+
+// use of map
+
+const map=new Map()
+map.set('IN' , "India")
+map.set('USA' , "United staes of america")
+map.set('UK' , "United Kindom")
+console.log(map)
+
+for(const [key , value] of map){
+    console.log(key + " :- " + value)
+}
