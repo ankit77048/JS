b2f08008952778c8e1654c4100036f7260bb50c0
Ankit Yadav <ankit.official7704@gmail.com>
2026-02-15 11:10:23 +0000

    getter

---
+++ OOPs/getterSetter.js
@@ -0,0 +1,21 @@
+class user{
+    constructor(email,password){
+        this.email=email
+        this.password=password
+    }
+    get email(){
+       return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email=value
+    }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password=value
+    }
+}
+const obj=new user("abc123@gmail.com","abc")
+console.log(obj.password)
+console.log(obj.email)
